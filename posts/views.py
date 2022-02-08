68dd8748fbce9f089b8ce330e03d64f473f63b64
francis556 <vikrammehra244@gmail.com>
2022-02-08 18:18:12 +0200

    created CRUDL class views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, PostView, Comment, Like
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = (
+        'title',
+        'content',
+        'thumbnail',
+        'author',
+        'slug',
+    )
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = (
+        'title',
+        'content',
+        'thumbnail',
+        'author',
+        'slug',
+    )
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
