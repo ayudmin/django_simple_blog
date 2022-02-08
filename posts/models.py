1f19dac0206eca54f1201af18bed85340991df1a
francis556 <vikrammehra244@gmail.com>
2022-02-08 19:42:13 +0200

    styling 😻😻

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.shortcuts import reverse
 
 
 class User(AbstractUser):
@@ -20,6 +21,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={
+            'slug': self.slug
+        })
 
 
 class Comment(models.Model):
