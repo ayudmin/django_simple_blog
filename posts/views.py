bc4e512fcc649cf0aee4ef1003f9a99946b2a0ef
francis556 <vikrammehra244@gmail.com>
2022-02-08 19:14:44 +0200

    added success url on view [C|D] and get_context_data() function

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, PostView, Comment, Like
+from .forms import PostForm
 
 
 class PostListView(ListView):
@@ -9,13 +10,15 @@ class PostListView(ListView):
 
 class PostCreateView(CreateView):
     model = Post
-    fields = (
-        'title',
-        'content',
-        'thumbnail',
-        'author',
-        'slug',
-    )
+    form_class = PostForm
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'view_type': 'create'
+        })
+        return context
 
 
 class PostDetailView(DetailView):
@@ -24,13 +27,15 @@ class PostDetailView(DetailView):
 
 class PostUpdateView(UpdateView):
     model = Post
-    fields = (
-        'title',
-        'content',
-        'thumbnail',
-        'author',
-        'slug',
-    )
+    form_class = PostForm
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'view_type': 'update'
+        })
+        return context
 
 
 class PostDeleteView(DeleteView):
