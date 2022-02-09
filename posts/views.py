373988fe22a7500fbf8d2eaceda6494702f8056d
francis556 <vikrammehra244@gmail.com>
2022-02-09 16:25:03 +0200

    added like view

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, PostView, Comment, Like
 from .forms import PostForm
@@ -41,3 +41,17 @@ class PostUpdateView(UpdateView):
 class PostDeleteView(DeleteView):
     model = Post
     success_url = '/'
+
+
+def like(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    like_qs = Like.objects.filter(user=request.user, post=post)
+    if like_qs.exists():
+        # unlike post
+        like_qs[0].delete()
+        return redirect('detail', slug=slug)
+    else:
+        new_like = Like.objects.create(user=request.user, post=post)
+        return redirect('detail', slug=slug)
+
+
