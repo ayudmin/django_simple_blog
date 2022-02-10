da3f668bd0cddb298418b08ce3b0c4dc0df5bb0c
francis556 <vikrammehra244@gmail.com>
2022-02-10 23:07:26 +0200

    added crisp_forms config

--- blog/settings.py
+++ blog/settings.py
@@ -28,13 +28,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
 
     'posts',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
